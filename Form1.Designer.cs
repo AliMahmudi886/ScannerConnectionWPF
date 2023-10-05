7ecfdd445809d494e705478016585af265ef4c78
Ali <ali.mahmudi.fard01@gmail.com>
2023-10-05 18:18:19 +0330

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace ScannerConnection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ScannerList = new System.Windows.Forms.ComboBox();
+            this.btnScanner = new System.Windows.Forms.Button();
+            this.picScanImage = new System.Windows.Forms.PictureBox();
+            this.lblConnectionStatus = new System.Windows.Forms.Label();
+            this.btnSaveAsPdf = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picScanImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ScannerList
+            // 
+            this.ScannerList.FormattingEnabled = true;
+            this.ScannerList.Location = new System.Drawing.Point(500, 25);
+            this.ScannerList.Name = "ScannerList";
+            this.ScannerList.Size = new System.Drawing.Size(288, 21);
+            this.ScannerList.TabIndex = 0;
+            this.ScannerList.SelectedIndexChanged += new System.EventHandler(this.ScannerList_SelectedIndexChanged);
+            // 
+            // btnScanner
+            // 
+            this.btnScanner.Location = new System.Drawing.Point(12, 12);
+            this.btnScanner.Name = "btnScanner";
+            this.btnScanner.Size = new System.Drawing.Size(75, 23);
+            this.btnScanner.TabIndex = 1;
+            this.btnScanner.Text = "Scan";
+            this.btnScanner.UseVisualStyleBackColor = true;
+            this.btnScanner.Click += new System.EventHandler(this.btnScanner_Click);
+            // 
+            // picScanImage
+            // 
+            this.picScanImage.Location = new System.Drawing.Point(12, 79);
+            this.picScanImage.Name = "picScanImage";
+            this.picScanImage.Size = new System.Drawing.Size(776, 359);
+            this.picScanImage.TabIndex = 2;
+            this.picScanImage.TabStop = false;
+            // 
+            // lblConnectionStatus
+            // 
+            this.lblConnectionStatus.AutoSize = true;
+            this.lblConnectionStatus.Location = new System.Drawing.Point(659, 9);
+            this.lblConnectionStatus.Name = "lblConnectionStatus";
+            this.lblConnectionStatus.Size = new System.Drawing.Size(35, 13);
+            this.lblConnectionStatus.TabIndex = 3;
+            this.lblConnectionStatus.Text = "label1";
+            // 
+            // btnSaveAsPdf
+            // 
+            this.btnSaveAsPdf.Location = new System.Drawing.Point(12, 50);
+            this.btnSaveAsPdf.Name = "btnSaveAsPdf";
+            this.btnSaveAsPdf.Size = new System.Drawing.Size(75, 23);
+            this.btnSaveAsPdf.TabIndex = 4;
+            this.btnSaveAsPdf.Text = "Save As Pdf";
+            this.btnSaveAsPdf.UseVisualStyleBackColor = true;
+            this.btnSaveAsPdf.Click += new System.EventHandler(this.btnSaveAsPdf_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSaveAsPdf);
+            this.Controls.Add(this.lblConnectionStatus);
+            this.Controls.Add(this.picScanImage);
+            this.Controls.Add(this.btnScanner);
+            this.Controls.Add(this.ScannerList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picScanImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox ScannerList;
+        private System.Windows.Forms.Button btnScanner;
+        private System.Windows.Forms.PictureBox picScanImage;
+        private System.Windows.Forms.Label lblConnectionStatus;
+        private System.Windows.Forms.Button btnSaveAsPdf;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Windows.Forms;
+using WIA;
+using System.IO;
+using WIACommonDialog = WIA.CommonDialog;
+using System.Collections.Generic;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.Collections.Generic;
+using System.Drawing;
+using Image = System.Drawing.Image;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ScannerConnection
+{
+    public partial class Form1 : Form
+    {
+        DeviceManager deviceManager = new DeviceManager();
+        DeviceInfo firstScannerAvailable = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckScannerConnection();
+            PopulateScannerList();
+        }
+        private List<DeviceInfo> ListAvailableScanners()
+        {
+            List<DeviceInfo> scanners = new List<DeviceInfo>();
+            DeviceManager manager = new DeviceManager();
+
+            foreach (DeviceInfo deviceInfo in manager.DeviceInfos)
+            {
+                if (deviceInfo.Type == WiaDeviceType.ScannerDeviceType)
+                {
+                    scanners.Add(deviceInfo);
+                }
+            }
+
+            return scanners;
+        }
+        private void PopulateScannerList()
+        {
+            List<DeviceInfo> scanners = ListAvailableScanners();
+
+            // Clear the list (in case of updates)
+            ScannerList.Items.Clear();
+
+            // Add the scanner names to the list
+            foreach (DeviceInfo scanner in scanners)
+            {
+                ScannerList.Items.Add(scanner.Properties["Name"].get_Value());
+            }
+        }
+        private Device ConnectToSelectedScanner()
+        {
+            if (ScannerList.SelectedIndex >= 0)
+            {
+                DeviceManager manager = new DeviceManager();
+                DeviceInfo selectedDeviceInfo = ListAvailableScanners()[ScannerList.SelectedIndex];
+                Device connectedScanner = selectedDeviceInfo.Connect();
+                return connectedScanner;
+            }
+            else
+            {
+                MessageBox.Show("Chooose a scanner");
+                return null;
+            }
+        }
+
+
+        private void btnScanner_Click(object sender, EventArgs e)
+        {
+
+            List<Image> images = new List<Image>();
+            Device connectedScanner = ConnectToSelectedScanner();
+
+            if (connectedScanner != null && connectedScanner.Items.Count>0)
+            {
+                try
+                {
+                    WIA.Item item = connectedScanner.Items[1] as WIA.Item;
+                    try
+                    {
+                        const string wiaFormatBMP = "{B96B3CAB-0728-11D3-9D7B-0000F81EF32E}";
+                        // scan image
+                        WIA.ICommonDialog wiaCommonDialog = new WIA.CommonDialog();
+                        WIA.ImageFile image = (WIA.ImageFile)wiaCommonDialog.ShowTransfer(item, wiaFormatBMP, false);
+
+                        // save to temp file
+                        string fileName = Path.GetTempFileName();
+                        File.Delete(fileName);
+                        image.SaveFile(fileName);
+                        image = null;
+                        // add file to output list
+                        images.Add(Image.FromFile(fileName));
+                        // Load the scanned image from the temporary file
+                        Image scannedImage = Image.FromFile(fileName);
+
+                        // Display the scanned image in the PictureBox
+                        picScanImage.Image = scannedImage;
+
+                        // Add the scanned image to the list of images (if needed)
+                        images.Add(scannedImage);
+                    }
+                    catch (Exception exc)
+                    {
+                        throw exc;
+                    }
+
+
+
+
+                   
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("An error occurred: " + ex.Message);
+                }
+            }
+        }
+        private void CheckScannerConnection()
+        {
+            List<DeviceInfo> scanners = ListAvailableScanners();
+
+            if (scanners.Count > 0)
+            {
+                // At least one scanner is connected
+                lblConnectionStatus.Text = "Scanner is Connected";
+            }
+            else
+            {
+                // No scanners are connected
+                lblConnectionStatus.Text = "Scanner is not Connected";
+            }
+        }
+        private void ScannerList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSaveAsPdf_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (picScanImage.Image != null)
+                {
+                    // Define the folder path where you want to save the PDF files
+                    string folderPath = @"D:\Scanned-Documents\";
+
+                    // Create the folder if it doesn't exist
+                    Directory.CreateDirectory(folderPath);
+
+                    // Generate a unique PDF file name based on timestamp
+                    string pdfFileName = Path.Combine(folderPath, DateTime.Now.ToString("yyyyMMddHHmmss") + ".pdf");
+
+                    // Initialize a FileStream to write the PDF file
+                    using (FileStream fs = new FileStream(pdfFileName, FileMode.Create))
+                    {
+                        Document pdfDocument = new Document();
+                        PdfWriter writer = PdfWriter.GetInstance(pdfDocument, fs);
+                        pdfDocument.Open();
+
+                        // Create an image and add it to the PDF document
+                        using (MemoryStream stream = new MemoryStream())
+                        {
+                            picScanImage.Image.Save(stream, System.Drawing.Imaging.ImageFormat.Jpeg);
+                            iTextSharp.text.Image pdfImage = iTextSharp.text.Image.GetInstance(stream.GetBuffer());
+                            pdfDocument.Add(pdfImage);
+                        }
+
+                        pdfDocument.Close();
+                    }
+
+                    MessageBox.Show("Scanned document saved as PDF: " + pdfFileName);
+                }
+                else
+                {
+                    MessageBox.Show("No scanned image available to save as PDF.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
+        }
+    }
+}
